bc6ff8f90e3b046029565f2b95e0d4b6d2703505
takatea <takat.fb.xavi@gmail.com>
2024-12-09 23:08:24 +0900

    導線追加

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,8 @@ export const Route = createRootRoute({
     <>
       <div>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |
-        <Link to="/chakra-ui">chakraUi</Link> |<Link to="/maps">GoogleMap</Link>
+        <Link to="/chakra-ui">chakraUi</Link> |<Link to="/maps">GoogleMap</Link>{" "}
+        |<Link to="/directionMaps">GoogleMap (direction)</Link>
       </div>
       <hr />
       <Outlet />
