81a3c471c0771a938e4c8124d0cef9c6b144dca0
takatea <takat.fb.xavi@gmail.com>
2025-01-13 15:51:26 +0900

    chore: router自動生成

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,171 +8,220 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
+import { createFileRoute } from '@tanstack/react-router'
+
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
-import { Route as IndexImport } from './routes/index'
-import { Route as MapsIndexImport } from './routes/maps/index'
-import { Route as DirectionMapsIndexImport } from './routes/directionMaps/index'
-import { Route as ChakraUiIndexImport } from './routes/chakra-ui/index'
-import { Route as AgentIndexImport } from './routes/agent/index'
-import { Route as AboutIndexImport } from './routes/about/index'
+import { Route as SamplesImport } from './routes/samples'
+import { Route as SamplesIndexImport } from './routes/samples/index'
+import { Route as SamplesChakraUiIndexImport } from './routes/samples/chakra-ui/index'
+import { Route as SamplesAboutIndexImport } from './routes/samples/about/index'
+
+// Create Virtual Routes
+
+const SamplesMapsIndexLazyImport = createFileRoute('/samples/maps/')()
+const SamplesDirectionMapsIndexLazyImport = createFileRoute(
+  '/samples/directionMaps/',
+)()
+const SamplesAgentIndexLazyImport = createFileRoute('/samples/agent/')()
 
 // Create/Update Routes
 
-const IndexRoute = IndexImport.update({
+const SamplesRoute = SamplesImport.update({
+  id: '/samples',
+  path: '/samples',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const SamplesIndexRoute = SamplesIndexImport.update({
   id: '/',
   path: '/',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => SamplesRoute,
 } as any)
 
-const MapsIndexRoute = MapsIndexImport.update({
+const SamplesMapsIndexLazyRoute = SamplesMapsIndexLazyImport.update({
   id: '/maps/',
   path: '/maps/',
-  getParentRoute: () => rootRoute,
-} as any)
+  getParentRoute: () => SamplesRoute,
+} as any).lazy(() =>
+  import('./routes/samples/maps/index.lazy').then((d) => d.Route),
+)
 
-const DirectionMapsIndexRoute = DirectionMapsIndexImport.update({
-  id: '/directionMaps/',
-  path: '/directionMaps/',
-  getParentRoute: () => rootRoute,
-} as any)
+const SamplesDirectionMapsIndexLazyRoute =
+  SamplesDirectionMapsIndexLazyImport.update({
+    id: '/directionMaps/',
+    path: '/directionMaps/',
+    getParentRoute: () => SamplesRoute,
+  } as any).lazy(() =>
+    import('./routes/samples/directionMaps/index.lazy').then((d) => d.Route),
+  )
 
-const ChakraUiIndexRoute = ChakraUiIndexImport.update({
-  id: '/chakra-ui/',
-  path: '/chakra-ui/',
-  getParentRoute: () => rootRoute,
-} as any)
-
-const AgentIndexRoute = AgentIndexImport.update({
+const SamplesAgentIndexLazyRoute = SamplesAgentIndexLazyImport.update({
   id: '/agent/',
   path: '/agent/',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => SamplesRoute,
+} as any).lazy(() =>
+  import('./routes/samples/agent/index.lazy').then((d) => d.Route),
+)
+
+const SamplesChakraUiIndexRoute = SamplesChakraUiIndexImport.update({
+  id: '/chakra-ui/',
+  path: '/chakra-ui/',
+  getParentRoute: () => SamplesRoute,
 } as any)
 
-const AboutIndexRoute = AboutIndexImport.update({
+const SamplesAboutIndexRoute = SamplesAboutIndexImport.update({
   id: '/about/',
   path: '/about/',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => SamplesRoute,
 } as any)
 
 // Populate the FileRoutesByPath interface
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
+    '/samples': {
+      id: '/samples'
+      path: '/samples'
+      fullPath: '/samples'
+      preLoaderRoute: typeof SamplesImport
+      parentRoute: typeof rootRoute
+    }
+    '/samples/': {
+      id: '/samples/'
       path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
+      fullPath: '/samples/'
+      preLoaderRoute: typeof SamplesIndexImport
+      parentRoute: typeof SamplesImport
     }
-    '/about/': {
-      id: '/about/'
+    '/samples/about/': {
+      id: '/samples/about/'
       path: '/about'
-      fullPath: '/about'
-      preLoaderRoute: typeof AboutIndexImport
-      parentRoute: typeof rootRoute
+      fullPath: '/samples/about'
+      preLoaderRoute: typeof SamplesAboutIndexImport
+      parentRoute: typeof SamplesImport
     }
-    '/agent/': {
-      id: '/agent/'
-      path: '/agent'
-      fullPath: '/agent'
-      preLoaderRoute: typeof AgentIndexImport
-      parentRoute: typeof rootRoute
-    }
-    '/chakra-ui/': {
-      id: '/chakra-ui/'
+    '/samples/chakra-ui/': {
+      id: '/samples/chakra-ui/'
       path: '/chakra-ui'
-      fullPath: '/chakra-ui'
-      preLoaderRoute: typeof ChakraUiIndexImport
-      parentRoute: typeof rootRoute
+      fullPath: '/samples/chakra-ui'
+      preLoaderRoute: typeof SamplesChakraUiIndexImport
+      parentRoute: typeof SamplesImport
     }
-    '/directionMaps/': {
-      id: '/directionMaps/'
+    '/samples/agent/': {
+      id: '/samples/agent/'
+      path: '/agent'
+      fullPath: '/samples/agent'
+      preLoaderRoute: typeof SamplesAgentIndexLazyImport
+      parentRoute: typeof SamplesImport
+    }
+    '/samples/directionMaps/': {
+      id: '/samples/directionMaps/'
       path: '/directionMaps'
-      fullPath: '/directionMaps'
-      preLoaderRoute: typeof DirectionMapsIndexImport
-      parentRoute: typeof rootRoute
+      fullPath: '/samples/directionMaps'
+      preLoaderRoute: typeof SamplesDirectionMapsIndexLazyImport
+      parentRoute: typeof SamplesImport
     }
-    '/maps/': {
-      id: '/maps/'
+    '/samples/maps/': {
+      id: '/samples/maps/'
       path: '/maps'
-      fullPath: '/maps'
-      preLoaderRoute: typeof MapsIndexImport
-      parentRoute: typeof rootRoute
+      fullPath: '/samples/maps'
+      preLoaderRoute: typeof SamplesMapsIndexLazyImport
+      parentRoute: typeof SamplesImport
     }
   }
 }
 
 // Create and export the route tree
 
+interface SamplesRouteChildren {
+  SamplesIndexRoute: typeof SamplesIndexRoute
+  SamplesAboutIndexRoute: typeof SamplesAboutIndexRoute
+  SamplesChakraUiIndexRoute: typeof SamplesChakraUiIndexRoute
+  SamplesAgentIndexLazyRoute: typeof SamplesAgentIndexLazyRoute
+  SamplesDirectionMapsIndexLazyRoute: typeof SamplesDirectionMapsIndexLazyRoute
+  SamplesMapsIndexLazyRoute: typeof SamplesMapsIndexLazyRoute
+}
+
+const SamplesRouteChildren: SamplesRouteChildren = {
+  SamplesIndexRoute: SamplesIndexRoute,
+  SamplesAboutIndexRoute: SamplesAboutIndexRoute,
+  SamplesChakraUiIndexRoute: SamplesChakraUiIndexRoute,
+  SamplesAgentIndexLazyRoute: SamplesAgentIndexLazyRoute,
+  SamplesDirectionMapsIndexLazyRoute: SamplesDirectionMapsIndexLazyRoute,
+  SamplesMapsIndexLazyRoute: SamplesMapsIndexLazyRoute,
+}
+
+const SamplesRouteWithChildren =
+  SamplesRoute._addFileChildren(SamplesRouteChildren)
+
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/about': typeof AboutIndexRoute
-  '/agent': typeof AgentIndexRoute
-  '/chakra-ui': typeof ChakraUiIndexRoute
-  '/directionMaps': typeof DirectionMapsIndexRoute
-  '/maps': typeof MapsIndexRoute
+  '/samples': typeof SamplesRouteWithChildren
+  '/samples/': typeof SamplesIndexRoute
+  '/samples/about': typeof SamplesAboutIndexRoute
+  '/samples/chakra-ui': typeof SamplesChakraUiIndexRoute
+  '/samples/agent': typeof SamplesAgentIndexLazyRoute
+  '/samples/directionMaps': typeof SamplesDirectionMapsIndexLazyRoute
+  '/samples/maps': typeof SamplesMapsIndexLazyRoute
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/about': typeof AboutIndexRoute
-  '/agent': typeof AgentIndexRoute
-  '/chakra-ui': typeof ChakraUiIndexRoute
-  '/directionMaps': typeof DirectionMapsIndexRoute
-  '/maps': typeof MapsIndexRoute
+  '/samples': typeof SamplesIndexRoute
+  '/samples/about': typeof SamplesAboutIndexRoute
+  '/samples/chakra-ui': typeof SamplesChakraUiIndexRoute
+  '/samples/agent': typeof SamplesAgentIndexLazyRoute
+  '/samples/directionMaps': typeof SamplesDirectionMapsIndexLazyRoute
+  '/samples/maps': typeof SamplesMapsIndexLazyRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
-  '/': typeof IndexRoute
-  '/about/': typeof AboutIndexRoute
-  '/agent/': typeof AgentIndexRoute
-  '/chakra-ui/': typeof ChakraUiIndexRoute
-  '/directionMaps/': typeof DirectionMapsIndexRoute
-  '/maps/': typeof MapsIndexRoute
+  '/samples': typeof SamplesRouteWithChildren
+  '/samples/': typeof SamplesIndexRoute
+  '/samples/about/': typeof SamplesAboutIndexRoute
+  '/samples/chakra-ui/': typeof SamplesChakraUiIndexRoute
+  '/samples/agent/': typeof SamplesAgentIndexLazyRoute
+  '/samples/directionMaps/': typeof SamplesDirectionMapsIndexLazyRoute
+  '/samples/maps/': typeof SamplesMapsIndexLazyRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | '/'
-    | '/about'
-    | '/agent'
-    | '/chakra-ui'
-    | '/directionMaps'
-    | '/maps'
+    | '/samples'
+    | '/samples/'
+    | '/samples/about'
+    | '/samples/chakra-ui'
+    | '/samples/agent'
+    | '/samples/directionMaps'
+    | '/samples/maps'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/about' | '/agent' | '/chakra-ui' | '/directionMaps' | '/maps'
+  to:
+    | '/samples'
+    | '/samples/about'
+    | '/samples/chakra-ui'
+    | '/samples/agent'
+    | '/samples/directionMaps'
+    | '/samples/maps'
   id:
     | '__root__'
-    | '/'
-    | '/about/'
-    | '/agent/'
-    | '/chakra-ui/'
-    | '/directionMaps/'
-    | '/maps/'
+    | '/samples'
+    | '/samples/'
+    | '/samples/about/'
+    | '/samples/chakra-ui/'
+    | '/samples/agent/'
+    | '/samples/directionMaps/'
+    | '/samples/maps/'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
-  AboutIndexRoute: typeof AboutIndexRoute
-  AgentIndexRoute: typeof AgentIndexRoute
-  ChakraUiIndexRoute: typeof ChakraUiIndexRoute
-  DirectionMapsIndexRoute: typeof DirectionMapsIndexRoute
-  MapsIndexRoute: typeof MapsIndexRoute
+  SamplesRoute: typeof SamplesRouteWithChildren
 }
 
 const rootRouteChildren: RootRouteChildren = {
-  IndexRoute: IndexRoute,
-  AboutIndexRoute: AboutIndexRoute,
-  AgentIndexRoute: AgentIndexRoute,
-  ChakraUiIndexRoute: ChakraUiIndexRoute,
-  DirectionMapsIndexRoute: DirectionMapsIndexRoute,
-  MapsIndexRoute: MapsIndexRoute,
+  SamplesRoute: SamplesRouteWithChildren,
 }
 
 export const routeTree = rootRoute
@@ -185,31 +234,43 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/",
-        "/about/",
-        "/agent/",
-        "/chakra-ui/",
-        "/directionMaps/",
-        "/maps/"
+        "/samples"
       ]
     },
-    "/": {
-      "filePath": "index.tsx"
+    "/samples": {
+      "filePath": "samples.tsx",
+      "children": [
+        "/samples/",
+        "/samples/about/",
+        "/samples/chakra-ui/",
+        "/samples/agent/",
+        "/samples/directionMaps/",
+        "/samples/maps/"
+      ]
     },
-    "/about/": {
-      "filePath": "about/index.tsx"
+    "/samples/": {
+      "filePath": "samples/index.tsx",
+      "parent": "/samples"
     },
-    "/agent/": {
-      "filePath": "agent/index.tsx"
+    "/samples/about/": {
+      "filePath": "samples/about/index.tsx",
+      "parent": "/samples"
     },
-    "/chakra-ui/": {
-      "filePath": "chakra-ui/index.tsx"
+    "/samples/chakra-ui/": {
+      "filePath": "samples/chakra-ui/index.tsx",
+      "parent": "/samples"
     },
-    "/directionMaps/": {
-      "filePath": "directionMaps/index.tsx"
+    "/samples/agent/": {
+      "filePath": "samples/agent/index.lazy.tsx",
+      "parent": "/samples"
     },
-    "/maps/": {
-      "filePath": "maps/index.tsx"
+    "/samples/directionMaps/": {
+      "filePath": "samples/directionMaps/index.lazy.tsx",
+      "parent": "/samples"
+    },
+    "/samples/maps/": {
+      "filePath": "samples/maps/index.lazy.tsx",
+      "parent": "/samples"
     }
   }
 }
